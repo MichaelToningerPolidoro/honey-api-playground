491a87b6e9f942916786a0d34807e8c422cf5438
Michael Toninger <michaeltp.2012@hotmail.com>
2022-03-03 17:55:25 -0300

    First commit

---
+++ src/test/java/com/honeyautomation/apiplayground/ApiplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeyautomation.apiplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
