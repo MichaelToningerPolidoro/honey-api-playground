91886986f49693eca25f260745d846c9559de912
Michael Toninger <michaeltp.2012@hotmail.com>
2022-03-25 17:38:29 -0300

    Implementado o Controller do Hobby

---
+++ src/main/java/com/honeyautomation/apiplayground/controllers/HobbyController.java
@@ -0,0 +1,22 @@
+package com.honeyautomation.apiplayground.controllers;
+
+import com.honeyautomation.apiplayground.dto.HobbyDTO;
+import com.honeyautomation.apiplayground.services.HobbyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hobbies")
+public class HobbyController {
+
+    @Autowired
+    private HobbyService hobbyService;
+
+    @GetMapping
+    public ResponseEntity<HobbyDTO> findAll() {
+        return ResponseEntity.ok(hobbyService.findAll());
+    }
+}
