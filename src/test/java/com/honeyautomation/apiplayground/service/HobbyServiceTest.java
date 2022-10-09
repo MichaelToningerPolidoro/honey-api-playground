9fc2f62979678e78921eade569ce12d0d6aed3a3
Michael Toninger <michaeltp.2012@hotmail.com>
2022-10-09 16:52:56 -0300

    Addes unit tests for hobbies service

---
+++ src/test/java/com/honeyautomation/apiplayground/service/HobbyServiceTest.java
@@ -0,0 +1,52 @@
+package com.honeyautomation.apiplayground.service;
+
+import com.honeyautomation.apiplayground.domain.Hobby;
+import com.honeyautomation.apiplayground.exception.ItemNotFoundException;
+import com.honeyautomation.apiplayground.repository.HobbyRepository;
+import com.honeyautomation.apiplayground.response.HobbyResponseDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+class HobbyServiceTest {
+
+    @InjectMocks
+    private HobbyService hobbyService;
+    @Mock
+    private HobbyRepository hobbyRepositoryMock;
+
+    @Test
+    @DisplayName("Find all hobbies should return successfully")
+    void findAllHobbiesShouldReturnAList() {
+        final String hobbyName = "Programming";
+        Mockito.when(hobbyRepositoryMock.findAll()).thenReturn(List.of(new Hobby(1, hobbyName)));
+
+        Assertions.assertDoesNotThrow(() -> {
+            hobbyService.findAll();
+        }, ItemNotFoundException.class.getSimpleName());
+
+        final HobbyResponseDTO allHobbies = hobbyService.findAll();
+
+        Assertions.assertNotNull(allHobbies);
+        Assertions.assertNotNull(allHobbies.getHobbies());
+        Assertions.assertEquals(allHobbies.getHobbies().size(), 1);
+        Assertions.assertTrue(allHobbies.getHobbies().contains(hobbyName));
+    }
+
+    @Test
+    @DisplayName("Should throw ItemNotFoundException when no hobbies were found")
+    void findAllHobbiesShouldThrownItemNotFoundExceptionWhenNoHobbiesWereFound() {
+        Mockito.when(hobbyRepositoryMock.findAll()).thenReturn(Collections.emptyList());
+
+        Assertions.assertThrows(ItemNotFoundException.class, () -> hobbyService.findAll());
+    }
+}
