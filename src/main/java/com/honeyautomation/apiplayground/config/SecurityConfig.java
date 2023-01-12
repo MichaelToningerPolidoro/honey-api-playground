e8af2887023d116ea30ab91540952ee48258429c
Michael Toninger <michaeltp.2012@hotmail.com>
2023-01-12 18:06:48 -0300

    Added a DefaultPasswordEncoder

--- src/main/java/com/honeyautomation/apiplayground/config/SecurityConfig.java
+++ src/main/java/com/honeyautomation/apiplayground/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.honeyautomation.apiplayground.config;
 
+import com.honeyautomation.apiplayground.encoder.DefaultPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -22,8 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        
+        final PasswordEncoder passwordEncoder = DefaultPasswordEncoder.getDefaultEncoder();
         auth.inMemoryAuthentication()
                 .withUser("HoneyApiPlayground")
                 .password(passwordEncoder.encode("HoneyBasicPassword"))
---
+++ src/main/java/com/honeyautomation/apiplayground/encoder/DefaultPasswordEncoder.java
@@ -0,0 +1,15 @@
+package com.honeyautomation.apiplayground.encoder;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class DefaultPasswordEncoder {
+
+    // This strength value could be changed, but it is for learning purpose only ..
+    private static final int STRENGTH = 10;
+    private static final PasswordEncoder DEFAULT_ENCODER = new BCryptPasswordEncoder(STRENGTH);
+
+    public static PasswordEncoder getDefaultEncoder() {
+        return DEFAULT_ENCODER;
+    }
+}
