75e8465269d757db98f11b8bb7f640fe4ac3cadb
Michael Toninger <michaeltp.2012@hotmail.com>
2023-01-13 17:27:39 -0300

    Removed unused lombok annotations from Hobby classes

--- src/main/java/com/honeyautomation/apiplayground/domain/Hobby.java
+++ src/main/java/com/honeyautomation/apiplayground/domain/Hobby.java
@@ -1,16 +1,10 @@
 package com.honeyautomation.apiplayground.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tb_hobbies")
 public class Hobby {
--- src/main/java/com/honeyautomation/apiplayground/response/HobbyResponseDTO.java
+++ src/main/java/com/honeyautomation/apiplayground/response/HobbyResponseDTO.java
@@ -2,19 +2,15 @@ package com.honeyautomation.apiplayground.response;
 
 import com.honeyautomation.apiplayground.domain.Hobby;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-@Setter
-@NoArgsConstructor
 public class HobbyResponseDTO implements Serializable {
 
-    private List<String> hobbies;
+    private final List<String> hobbies;
 
     public HobbyResponseDTO(List<Hobby> hobbies) {
         this.hobbies = hobbies.stream().map(Hobby::getHobby).collect(Collectors.toList());
