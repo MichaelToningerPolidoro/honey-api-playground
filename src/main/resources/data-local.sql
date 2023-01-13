d22a44a154088e3d07d68d85d9e917cfb0b3fb97
Michael Toninger <michaeltp.2012@hotmail.com>
2023-01-13 17:24:01 -0300

    Added standard values for local profile database

--- src/main/resources/data-local.sql
+++ src/main/resources/data-local.sql
@@ -1,2 +1,4 @@
 insert into tb_hobbies(hobby) values ('Programming'), ('Watch movies'), ('Watch series'), ('Play video games'),
 ('Practice sports'), ('Ride a bicycle');
+insert into tb_programming_time_options(programming_time) values ('Less than a month'), ('Between 1 and 6 months'),
+('Between 6 months and 1 year'), ('Between 1 and 2 years'), ('More than 2 years');
