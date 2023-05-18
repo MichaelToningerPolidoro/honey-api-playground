de120191d43e9bcc2190a59e67b85e81d9c0f512
Michael Toninger <michaeltp.2012@hotmail.com>
2023-05-18 11:51:55 -0300

    Added internal server error test to UserController

--- src/test/java/com/honeyautomation/apiplayground/controller/UserControllerTest.java
+++ src/test/java/com/honeyautomation/apiplayground/controller/UserControllerTest.java
@@ -1,20 +1,33 @@
 package com.honeyautomation.apiplayground.controller;
 
+import com.honeyautomation.apiplayground.constants.Endpoints;
+import com.honeyautomation.apiplayground.constants.ExceptionMessages;
+import com.honeyautomation.apiplayground.creator.MockMvcCreator;
+import com.honeyautomation.apiplayground.creator.RegisterRequestDTOCreator;
+import com.honeyautomation.apiplayground.exception.TestException;
 import com.honeyautomation.apiplayground.service.UserService;
+import com.honeyautomation.apiplayground.utils.JsonParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static com.honeyautomation.apiplayground.creator.RegisterRequestDTOCreator.validRegisterRequestDTO;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 public class UserControllerTest {
@@ -34,5 +47,24 @@ public class UserControllerTest {
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertNull(response.getBody());
+    }
+
+    @Test
+    @DisplayName("User controller should thrown internal server error when unexpected error occurs")
+    void userControllerShouldThrownInternalServerErrorWhenUnexpectedErrorOccurs() throws Exception {
+        final MockMvc mockMvc = MockMvcCreator.create(userController);
+
+        doThrow(new TestException()).when(userServiceMock).create(any());
+
+        final MockHttpServletRequestBuilder request = post(Endpoints.REQUEST_MAPPING_USER)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonParser.parse(RegisterRequestDTOCreator.validRegisterRequestDTO()));
+
+        mockMvc.perform(request)
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("message", is(ExceptionMessages.INTERNAL_SERVER_ERROR)))
+        ;
+
     }
 }
--- src/test/java/com/honeyautomation/apiplayground/repository/UserRepositoryTest.java
+++ src/test/java/com/honeyautomation/apiplayground/repository/UserRepositoryTest.java
@@ -3,7 +3,6 @@ package com.honeyautomation.apiplayground.repository;
 
 import com.honeyautomation.apiplayground.creator.UserCreator;
 import com.honeyautomation.apiplayground.domain.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,9 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,32 +22,35 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    private static final User userToSaveBeforeTests = UserCreator.validUser();
+    @Autowired
+    private CountryRepository countryRepository;
 
-    private final Set<User> userDataSet = new HashSet<>();
+    @Autowired
+    private HobbyRepository hobbyRepository;
 
-    @BeforeEach
-    void beforeEach() {
-        userRepository.deleteAll();
-
-        userDataSet.add(userToSaveBeforeTests);
-        userRepository.saveAll(userDataSet);
-    }
+    @Autowired
+    private ProgrammingTimeOptionRepository programmingTimeOptionRepository;
 
     @Test
-    @DisplayName("Retrieve all values from database should return all users successfully")
-    void findAllShouldReturnAllUsersSuccessfully() {
-        final List<User> usersRetrievedFromDatabase = userRepository.findAll();
+    @DisplayName("Find should return user successfully")
+    void findShouldReturnUserSuccessfully() {
+        final User userToSaveBeforeSearch = UserCreator.validUser();
+        insertForeignValuesToUser(userToSaveBeforeSearch);
+        final User savedUser = userRepository.save(userToSaveBeforeSearch);
 
-        assertNotNull(usersRetrievedFromDatabase);
-        assertFalse(usersRetrievedFromDatabase.isEmpty());
-        assertTrue(usersRetrievedFromDatabase.contains(userToSaveBeforeTests));
+        final Optional<User> retrievedUserFromDatabase = userRepository.findById(savedUser.getId());
+
+        assertNotNull(retrievedUserFromDatabase);
+        assertTrue(retrievedUserFromDatabase.isPresent());
+        assertEquals(savedUser.getName(), retrievedUserFromDatabase.get().getName());
     }
 
     @Test
     @DisplayName("Saving new user should be successfully inserted")
     void savingNewUserShouldBeSuccessfullyInserted() {
         final User userToSave = UserCreator.validUser();
+        insertForeignValuesToUser(userToSave);
+
         final User savedUser = userRepository.save(userToSave);
 
         assertNotNull(savedUser);
@@ -60,14 +60,20 @@ public class UserRepositoryTest {
     @ParameterizedTest(name = "Saving new user should thrown constraint violation")
     @MethodSource("constraintViolationProvidedParameters")
     void savingNewUserShouldThrowConstraintViolationException(User user) {
+        insertForeignValuesToUser(user);
         assertThrows(DataIntegrityViolationException.class, () -> userRepository.save(user));
     }
 
     private static Arguments[] constraintViolationProvidedParameters() {
         return new Arguments[] {
-                Arguments.of(userToSaveBeforeTests),
                 Arguments.of(UserCreator.userWithNullNickNameValue()),
                 Arguments.of(UserCreator.userWithNickNameTooBig())
         };
+    }
+
+    private void insertForeignValuesToUser(User user) {
+        countryRepository.save(user.getBornData().getCountry());
+        hobbyRepository.saveAll(user.getHobbies());
+        programmingTimeOptionRepository.save(user.getProgrammingTimeOption());
     }
 }
