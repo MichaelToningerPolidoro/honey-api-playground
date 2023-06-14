4b3a387756f7e50edd3f13d898be554691ceeab7
Michael Toninger <michaeltp.2012@hotmail.com>
2023-06-14 12:04:22 -0300

    Adjusted country service tests

--- src/test/java/com/honeyautomation/apiplayground/service/CountryServiceTest.java
+++ src/test/java/com/honeyautomation/apiplayground/service/CountryServiceTest.java
@@ -4,6 +4,7 @@ import com.honeyautomation.apiplayground.creator.CountryCreator;
 import com.honeyautomation.apiplayground.domain.Country;
 import com.honeyautomation.apiplayground.dto.response.CountryResponseDTO;
 import com.honeyautomation.apiplayground.exception.type.ItemNotFoundException;
+import com.honeyautomation.apiplayground.exception.type.ItemNotRegisteredException;
 import com.honeyautomation.apiplayground.repository.CountryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -43,6 +45,27 @@ public class CountryServiceTest {
         assertFalse(allCountriesFound.getCountries().isEmpty());
         assertEquals(allCountriesFound.getCountries().size(), countryListMockData.size());
         assertTrue(allCountriesFound.getCountries().contains(countryMockData.getCountry()));
+    }
+
+    @Test
+    @DisplayName("Find country should return a value successfully")
+    void findCountryShouldReturnAValueSuccessfully() {
+        final Country countryMock = CountryCreator.validCountry();
+
+        when(countryRepositoryMock.findByCountry(any())).thenReturn(countryMock);
+
+        final Country retrievedCountry = countryService.findCountry("any");
+
+        assertNotNull(retrievedCountry);
+        assertEquals(countryMock, retrievedCountry);
+    }
+
+    @Test
+    @DisplayName("Find no existent country should thrown ItemNotRegisteredException")
+    void findNoExistentCountryShouldThrownItemNotRegisteredException() {
+        when(countryRepositoryMock.findByCountry(any())).thenReturn(null);
+
+        assertThrows(ItemNotRegisteredException.class, () -> countryService.findCountry("any"));
     }
 
     @Test
