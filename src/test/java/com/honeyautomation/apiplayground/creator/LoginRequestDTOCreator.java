2a7f0aedfdd9d54f50675c55ad02c8d9a58e40de
Michael Toninger <michaeltp.2012@hotmail.com>
2023-08-02 20:18:15 -0300

    Added login service tests

---
+++ src/test/java/com/honeyautomation/apiplayground/creator/LoginRequestDTOCreator.java
@@ -0,0 +1,39 @@
+package com.honeyautomation.apiplayground.creator;
+
+import com.honeyautomation.apiplayground.dto.request.LoginRequestDTO;
+
+public final class LoginRequestDTOCreator {
+    
+    public static LoginRequestDTO validLoginRequestDTO() {
+        return LoginRequestDTO.builder()
+                .email(getValidEmail())
+                .password(getValidPassword())
+                .build();
+    }
+    
+    public static LoginRequestDTO invalidLoginRequestDTO() {
+        return LoginRequestDTO.builder()
+                .email(null)
+                .password(getValidPassword())
+                .build();
+    }
+
+    public static LoginRequestDTO invalidPasswordLoginRequestDTO() {
+        return LoginRequestDTO.builder()
+                .email(getValidEmail())
+                .password(getInvalidPassword())
+                .build();
+    }
+
+    private static String getValidEmail() {
+        return "validemail@testing.com";
+    }
+    
+    private static String getValidPassword() {
+        return "ValidPassword";
+    }
+
+    private static String getInvalidPassword() {
+        return "123987MyInvalidPassword987321";
+    }
+}
